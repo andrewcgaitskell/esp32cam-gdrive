0606c7f4823a81c16c23e18b8e6e756f6a759ed1
andrewcgaitskell <andrew@gaitskell.com>
2021-03-28 10:43:14 +0100

    Delete secrets.h

--- secrets.h
+++
@@ -1,7 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "My Very Secret SSID New One"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "My VS Password"	// replace MyPassword with your WiFi password
-
-#define SECRET_SCRIPT "My Secret Script"
