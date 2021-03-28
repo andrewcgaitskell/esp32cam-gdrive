78dba2ce01b99dec0803979ed88f85572d535253
andrewcgaitskell <andrew@gaitskell.com>
2021-03-28 10:47:41 +0100

    Create secrets_template.h

---
+++ secrets_template.h
@@ -0,0 +1,7 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "SSID"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "Password"	// replace MyPassword with your WiFi password
+
+#define SECRET_SCRIPT "/macros/s/XXXXXXXXXXXXXXXXXXXXXX/exec"
