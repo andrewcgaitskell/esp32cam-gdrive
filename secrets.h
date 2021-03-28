d80cb4718bb87b933da5886104098b3aef3fc35f
andrewcgaitskell <andrew@gaitskell.com>
2021-03-28 10:25:58 +0100

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -1,8 +1,6 @@
 // Use this file to store all of the private credentials 
 // and connection details
 
-#define SECRET_SSID "MySSID"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "MyPassword"	// replace MyPassword with your WiFi password
+#define SECRET_SSID "My Very Secret SSID"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "My VS Password"	// replace MyPassword with your WiFi password
 
-#define SECRET_CH_ID 000000			// replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "XYZ"   // replace XYZ with your channel write API Key
